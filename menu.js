3eabbe3171fe04ebc3ecb9ae21ae8490746163f6
kignas <sagnikroysagnikroy4@gmail.com>
2026-03-14 13:07:25 +0530

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,10 @@
+const restaurants = {
+  nirvana: {
+    name: "The Nirvana",
+    cuisine: "North Indian • Chinese • Mughlai",
+    rating: "3.9",
+    time: "20 – 25 mins",
+    distance: "5.4 km",
+    location: "Jalpaiguri Locality"
+  }
+};
